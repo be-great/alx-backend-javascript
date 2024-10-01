18329efa92cb01697b65fe15aaa9ca19583028c3
be-great <begreat5333@gmail.com>
2024-10-01 19:52:05 +0300

    update task 8

--- 0x00-ES6_basic/.eslintrc.js
+++ 0x00-ES6_basic/.eslintrc.js
@@ -1,28 +1,26 @@
 module.exports = {
-  env: {
-    browser: false,
-    es6: true,
-    jest: true,
-  },
-  extends: ["airbnb-base", "plugin:jest/all"],
-  globals: {
-    Atomics: "readonly",
-    SharedArrayBuffer: "readonly",
-  },
-  parserOptions: {
-    ecmaVersion: 2018,
-    sourceType: "module",
-  },
-  plugins: ["jest"],
-  rules: {
-    "no-console": "off",
-    "no-shadow": "off",
-    "no-restricted-syntax": ["error", "LabeledStatement", "WithStatement"],
-  },
-  overrides: [
-    {
-      files: ["*.js"],
-      excludedFiles: "babel.config.js",
+    "env": {
+        "browser": true,
+        "es6": true
     },
-  ],
+    "extends": [
+        "eslint:recommended",
+        "plugin:react/recommended"
+    ],
+    "globals": {
+        "Atomics": "readonly",
+        "SharedArrayBuffer": "readonly"
+    },
+    "parserOptions": {
+        "ecmaFeatures": {
+            "jsx": true
+        },
+        "ecmaVersion": 2018,
+        "sourceType": "module"
+    },
+    "plugins": [
+        "react"
+    ],
+    "rules": {
+    }
 };
