520cf221c9b6385da972c1be0e4e2b2c83cdb704
be-great <begreat5333@gmail.com>
2024-10-02 23:30:49 +0300

    update

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -4,10 +4,10 @@ import uploadPhoto from './5-photo-reject';
 export default async function handleProfileSignup(firstName, lastName, fileName) {
   return Promise
     .allSettled([signUpUser(firstName, lastName), uploadPhoto(fileName)])
-    .then((res) => (
-      res.map((o) => ({
-        status: o.status,
-        value: o.status === 'fulfilled' ? o.value : String(o.reason),
-      }))
-    ));
+    .then((res) => {
+      res.map((output) => ({
+        status: output.status,
+        value: output.status === 'fulfilled' ? output.value : String(output.reason),
+      }));
+    });
 }
