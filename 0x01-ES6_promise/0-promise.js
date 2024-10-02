4b7e6e8386ec20e748ece246c8ef43656b40c3ff
be-great <begreat5333@gmail.com>
2024-10-02 21:44:18 +0300

    update

--- 0x01-ES6_promise/0-promise.js
+++ 0x01-ES6_promise/0-promise.js
@@ -1,7 +1,6 @@
 export default function getResponseFromAPI() {
-
-    let myProm = new Promise((resolve) => {
-        resolve();
+    return new Promise((resolve) => {
+      resolve();
     });
-    return myProm;
-}
+  }
+  
