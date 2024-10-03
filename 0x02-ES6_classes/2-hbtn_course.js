9e8db59955938957475314530717625d19826aa1
be-great <begreat5333@gmail.com>
2024-10-03 20:43:07 +0300

    update task 2

--- 0x02-ES6_classes/2-hbtn_course.js
+++ 0x02-ES6_classes/2-hbtn_course.js
@@ -1,14 +1,30 @@
+/**
+ * A Course, Getters, and Setters.
+ */
 export default class ClassRoom {
+  /**
+   * Creates a new @see {@link HolbertonCourse}.
+   *
+   * String name - course name.
+   * Number length - length of the course (in months).
+   * String students - students name.
+   */
   constructor(name, length, students) {
     this._name = name;
     this._length = length;
     this._students = students;
   }
 
+  /**
+   * get name attribute.
+   */
   get name() {
     return this._name;
   }
 
+  /**
+   * set name attribute.
+   */
   set name(value) {
     if (typeof value !== 'string') {
       throw new TypeError('Name must be a string');
@@ -16,10 +32,16 @@ export default class ClassRoom {
     this._name = value;
   }
 
+  /**
+   * get length attribute.
+   */
   get length() {
     return this._length;
   }
 
+  /**
+   * set length attribute.
+   */
   set length(value) {
     if (typeof value !== 'number') {
       throw new TypeError('Length must be a number');
@@ -27,10 +49,16 @@ export default class ClassRoom {
     this._length = value;
   }
 
+  /**
+   * get students attribute.
+   */
   get students() {
     return this._students;
   }
 
+  /**
+   * set students attribute.
+   */
   set students(value) {
     if (!Array.isArray(value) !== !value.every((std) => typeof std === 'string')) {
       throw new TypeError('Students must be an array of strings');
