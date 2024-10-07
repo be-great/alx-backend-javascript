b6343848d95a0ce900374d2329cfb1ceb48f0439
be-great <begreat5333@gmail.com>
2024-10-07 23:47:14 +0300

    update

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -4,8 +4,8 @@ export default function cleanSet(set, startString) {
   }
 
   return [...set] // Convert the set to an array
-    .filter((value) => typeof value === 'string' && value.startsWith(startString)) // Filter strings that start with startString
-    .map((value) => value.slice(startString.length)) // Remove startString from each filtered value
-    .filter((valueSubStr) => valueSubStr) // Filter out empty or invalid substrings
+    .filter((str) => typeof str === 'string' && str.startsWith(startString)) // Filter strings that start with startString
+    .map((str) => str.slice(startString.length)) // Remove startString from each filtered value
+    .filter((str) => str) // Filter out empty or invalid substrings
     .join('-'); // Join remaining parts with a dash
 }
