67d9674ca3aeb2a07baa6f0ff7510d490e7da0e2
be-great <begreat5333@gmail.com>
2024-11-26 22:50:50 +0300

    update

--- 0x05-Node_JS_basic/2-read_file.js
+++ 0x05-Node_JS_basic/2-read_file.js
@@ -1,30 +1,30 @@
 /*
-Read file : 
+Read file :
 */
 const fs = require('fs');
+
 function countStudents(path) {
+  try {
+    const data = fs.readFileSync(path, 'utf8');
+    const lines = data.trim().split('\n');
+    const fields = {};
 
-    try {
-        const data = fs.readFileSync(path, 'utf8');
-        const lines = data.trim().split('\n');
-        const fields = {};
+    lines.slice(1).forEach((line) => {
+      if (line) {
+        const [firstname, , , field] = line.split(',');
+        if (!fields[field]) {
+          fields[field] = [];
+        }
+        fields[field].push(firstname);
+      }
+    });
 
-        lines.slice(1).forEach(line => {
-            if (line) {
-                const [firstname, , , field] = line.split(',');
-                if (!fields[field]) {
-                    fields[field] = [];
-                }
-                fields[field].push(firstname);
-            }
-        });
-
-        console.log(`Number of students: ${lines.length - 1}`);
-        Object.keys(fields).forEach(field => {
-            console.log(`Number of students in ${field}: ${fields[field].length}. List: ${fields[field].join(', ')}`);
-        });
-    } catch (error) {
-        throw new Error('Cannot load the database');
-    }
+    console.log(`Number of students: ${lines.length - 1}`);
+    Object.keys(fields).forEach((field) => {
+      console.log(`Number of students in ${field}: ${fields[field].length}. List: ${fields[field].join(', ')}`);
+    });
+  } catch (error) {
+    throw new Error('Cannot load the database');
+  }
 }
 module.exports = countStudents;
--- 0x05-Node_JS_basic/7-http_express.js
+++ 0x05-Node_JS_basic/7-http_express.js
@@ -4,15 +4,15 @@ const countStudents = require('./3-read_file_async');
 const app = express();
 
 app.get('/', (request, response) => {
-    response.send('Hello Holberton School!');
+  response.send('Hello Holberton School!');
 });
 app.get('/students', (request, response) => {
-    response.write('This is the list of our students\n');
-    countStudents(process.argv[2])
-        .then(() => response.end())
-        .catch(err => {
-            response.status(500).end(err.message);
-        });
+  response.write('This is the list of our students\n');
+  countStudents(process.argv[2])
+    .then(() => response.end())
+    .catch((err) => {
+      response.status(500).end(err.message);
+    });
 });
 app.listen(1245);
 module.exports = app;
