287ba98f7067f2e1cb76220497fb08dcd551c480
be-great <begreat5333@gmail.com>
2024-11-26 23:57:29 +0300

    update

--- 0x05-Node_JS_basic/5-http.js
+++ 0x05-Node_JS_basic/5-http.js
@@ -1,39 +1,21 @@
-const fs = require('fs');
+const http = require('http');
+const countStudents = require('./3-read_file_async');
 
-const fetchStudentData = (filePath) => new Promise((resolve, reject) => {
-  if (!filePath) {
-    reject(new Error('Cannot load the database'));
-    return;
+const app = http.createServer((req, res) => {
+  if (req.url === '/') {
+    res.writeHead(200, { 'Content-Type': 'text/plain' });
+    res.end('Hello Holberton School!');
+  } else if (req.url === '/students') {
+    res.writeHead(200, { 'Content-Type': 'text/plain' });
+    res.write('This is the list of our students\n');
+    countStudents(process.argv[2])
+      .then(() => res.end())
+      .catch((err) => {
+        res.end(err.message);
+      });
   }
-
-  fs.readFile(filePath, 'utf-8', (err, data) => {
-    if (err) {
-      reject(new Error('Cannot load the database'));
-      return;
-    }
-
-    const lines = data.trim().split('\n');
-    const groupByField = {};
-    const totalStudents = lines.slice(1).length;
-
-    lines.slice(1).forEach((line) => {
-      const studentInfo = line.split(',');
-      const field = studentInfo.pop();
-      if (!groupByField[field]) {
-        groupByField[field] = [];
-      }
-      groupByField[field].push(studentInfo[0]); // Assuming the first column is the name.
-    });
-
-    const report = [`Number of students: ${totalStudents}`];
-    for (const [field, students] of Object.entries(groupByField)) {
-      report.push(
-        `Number of students in ${field}: ${students.length}. List: ${students.join(', ')}`,
-      );
-    }
-
-    resolve(report.join('\n'));
-  });
 });
 
-module.exports = fetchStudentData; // Export the function for use in other files
+app.listen(1245);
+
+module.exports = app;
